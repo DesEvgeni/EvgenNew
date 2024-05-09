daf251a024bb0c63d57a0d44ec34afda9fe71ba5
a.taynov <alex.taynov@yandex.ru>
2024-05-09 23:45:10 +0300

    https api server

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-export const API_URL = `http://jsms.fisbi.ru/api/v1`
+export const API_URL = `https://api.jsms.cloud.taynov.ru/api/v1`
 
 const $api = axios.create({
     baseURL: API_URL
